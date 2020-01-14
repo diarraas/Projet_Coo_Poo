da432a30ed36fe72893357f4ee97251375610745
M. Mouret Quentin <qmouret@insa-11294>
2020-01-14 12:08:45 +0100

    Interface 14 01 matin

--- Chat System/src/GraphicUserInterface/Interface.java
+++ Chat System/src/GraphicUserInterface/Interface.java
@@ -9,31 +9,35 @@ import javax.swing.border.Border;
 
 public class Interface implements ActionListener {
 	
-	public static JPanel aff = new JPanel();
+	public static JPanel aff_border = new JPanel();
+	public static JPanel aff_inner = new JPanel();
 	public static JLabel aff_txt = new JLabel();
+	public static JLabel aff_nom = new JLabel(); 
 
-    public Interface() {
+    public Interface(String[] ponline) {
     	
     	//Create and set up the window.
         JFrame frame = new JFrame("Bonjour");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+        //frame.setResizable(false);
         Container pane = frame.getContentPane();
         
 	    JButton button;
 	    int index;
 		pane.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		String[] noms = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"};
+		String[] noms = ponline;
 		
 		
+		
+		//affichage label "People Online"
 		JLabel onliners = new JLabel("People Online :");
 		c.gridy = 0;
 		c.insets = new Insets(10,0,15,10);
 		c.anchor = GridBagConstraints.LINE_START;
 		pane.add(onliners,c);
 		
-		
+		//Création et ajout des boutons correspondant aux people connectés
 		for(index = 1; index<(noms.length);index++) {
 			
 			button = new JButton(noms[index-1]);	
@@ -52,38 +56,60 @@ public class Interface implements ActionListener {
 		c.gridy = index;	
 		pane.add(button,c);
 		
+		//Création label qui affichera le nom de la personne avec qui la session de clavardage est ouverte	
+		c.gridx = 1;
+		c.gridy = 0;
+		aff_nom.setText("");
+		pane.add(aff_nom);
 		
+		//Création zone de saisie de texte
 		JTextField text = new JTextField("");
 		c.gridx = 1;
 		c.gridy = index;
 		c.ipadx = 510;
 		c.ipady = 5;
-		c.anchor = GridBagConstraints.LINE_START;
-		pane.add(text,c);
+		pane.add(text,c);		
 		
-		
+		//Création zone d'affichage de la conversation		
 		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);	
-		aff.setBorder(border);
+		aff_border.setBorder(border);
+		aff_inner.setBorder(border);
+		
+		aff_border.setBounds(500,0,500, (noms.length * 410)/15 );
+		aff_border.setMaximumSize(new Dimension(500,(noms.length*410)/15));
+		
 		c.gridx = 1;
 		c.gridy = 1;
 		c.ipadx = 500;
 		c.ipady = (noms.length * 410)/15;
 		c.gridheight = (noms.length -1) ;
-		aff.add(aff_txt);
-		pane.add(aff,c);
+		aff_inner.add(aff_txt);
+		aff_border.add(aff_inner);
+		
+		pane.add(aff_border,c);
 		
 		frame.pack();
         frame.setVisible(true);
 	
-    }
+    } 
     
-    public void actionPerformed(ActionEvent e) {
-    	aff_txt.setText("Bonjour");    	
+    
+	public void actionPerformed(ActionEvent e) {
+		// Affichage de la personne avec qui on ouvre une session
+		aff_nom.setText("Conversation avec " + e.getActionCommand());
+		
+		aff_txt.setText(e.getActionCommand());
+    	
+		
+		aff_border.setSize(aff_border.getWidth()-13, aff_border.getHeight()-27);
+    	
+    	
     }
 
     public static void main(String[] args) {
- 
-        new Interface();
+    	
+    	String[] noms = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q"};
+        new Interface(noms);
     }
 }
 
